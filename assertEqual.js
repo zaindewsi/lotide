262750dd39db7d0742553703293001c2be0035b7
Zain Dewsi <zain.dewsi@gmail.com>
2021-04-26 18:10:31 +0000

    updated funtion with ES6 template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,14 @@
 const assertEqual = (actual, expected) => {
-  if (actual === expected) {
-    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
-  } else {
-    console.log("🛑🛑🛑 Asserion Failed: " + actual + " !== " + expected);
-  }
+
+  actual === expected ? console.log(`🐸🐸🐸 Assertion Passed: ${actual} === ${expected}`) : console.log(`😡😡😡 Assertion Failed: ${actual} !== ${expected}`);
+
+
+  // if (actual === expected) {
+  //   console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+  // } else {
+  //   console.log("🛑🛑🛑 Asserion Failed: " + actual + " !== " + expected);
+  // }
+
 };
 
 assertEqual("Lighthouse Labs", "Bootcamp");
