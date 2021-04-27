4199e08b67304e4d3f16d01771cf9ae4c5029540
Zain Dewsi <zain.dewsi@gmail.com>
2021-04-27 19:32:58 +0000

    added assertEqual to eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,3 @@
+const assertEqual = (actual, expected) => {
+  actual === expected ? console.log(`🐸🐸🐸 Assertion Passed: ${actual} === ${expected}`) : console.log(`😡😡😡 Assertion Failed: ${actual} !== ${expected}`);
+};
