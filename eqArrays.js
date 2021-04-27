30eac329dcba2fb89edbe7a316da866b7eb7f8ee
Zain Dewsi <zain.dewsi@gmail.com>
2021-04-27 19:54:56 +0000

    added eqArrays function

--- eqArrays.js
+++ eqArrays.js
@@ -1,3 +1,17 @@
 const assertEqual = (actual, expected) => {
   actual === expected ? console.log(`🐸🐸🐸 Assertion Passed: ${actual} === ${expected}`) : console.log(`😡😡😡 Assertion Failed: ${actual} !== ${expected}`);
 };
+
+const eqArrays = (arr1, arr2) => {
+ return arr1.length === arr2.length && arr1.every((item, i) => {
+    return item === arr2[i]})
+}
+
+
+console.log(eqArrays([1], [1])) // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 4])) // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
