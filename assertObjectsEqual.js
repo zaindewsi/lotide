026f2b61e098dc1dc68c78273d4b9cd5ce00f3e1
Zain Dewsi <zain.dewsi@gmail.com>
2021-04-28 19:47:20 +0000

    code refactor

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,14 +1,14 @@
 const eqObjects = (obj1, obj2) => {
-  const obj1KeysLength = Object.keys(obj1).length;
-  const obj2KeysLength = Object.keys(obj2).length;
+  const obj1Keys = Object.keys(obj1);
+  const obj2Keys = Object.keys(obj2);
 
-  if (obj1KeysLength !== obj2KeysLength) {
+  if (obj1Keys.length !== obj2Keys.length) {
     return false;
   }
 
-  for (const key in obj1) {
+  for (const key of obj1Keys) {
     if (Array.isArray(obj1[key])) {
-      if (!eqObjects(obj1[key], obj2[key])) {
+      if (!eqArrays(obj1[key], obj2[key])) {
         return false;
       }
     } else if (obj1[key] !== obj2[key]) {
