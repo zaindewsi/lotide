af4e6992b8fdf465a15a4dc8a387d9d64b111fc2
Zain Dewsi <zain.dewsi@gmail.com>
2021-04-28 18:27:41 +0000

    findKeyByValue linting

---
+++ findKeyByValue.js
@@ -0,0 +1,24 @@
+const assertEqual = (actual, expected) => {
+  actual === expected
+    ? console.log(`🐸🐸🐸 Assertion Passed: ${actual} === ${expected}`)
+    : console.log(`😡😡😡 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const findKeyByValue = (object, value) => {
+  const list = Object.keys(object);
+
+  for (let item of list) {
+    if (object[item] === value) {
+      return item;
+    }
+  }
+};
+
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
